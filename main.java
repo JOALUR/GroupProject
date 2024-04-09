5c114b2840115cbcdd3d941d135e31b23e98072f
ericsilva4206969 <165102987+ericsilva4206969@users.noreply.github.com>
2024-04-09 19:13:29 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class main {
 
 	public static void main(String[] args) {
   Scanner scanner = new Scanner(System.in);
@@ -33,6 +33,14 @@ public class Main {
         
         scanner.close();
     }
+public static void displayMenu(){
+	    System.out.println("Welcome to the Game Menu");
+            System.out.println("1. Rules");
+            System.out.println("2. Play Against Computer");
+            System.out.println("3. Play 2 Player Game");
+            System.out.println("4. See High Score");
+            System.out.println("5. Quit");
+            System.out.print("Enter your choice: ");
 
 	}
 
