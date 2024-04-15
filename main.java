f5d57c91d33177ef5f38826f8dcfd787ed3f4075
ericsilva4206969 <165102987+ericsilva4206969@users.noreply.github.com>
2024-04-15 09:17:19 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,107 +1,103 @@
+package aaaProject;
+
 import java.util.Scanner;
 import java.io.*;
 
-public class main {
+public class gameMain {
+
+	public static final String displayMenu = ("\nMain Menu" 
+	+ "\n1. Rules" 
+	+ "\n2. Play Against Computer"
+	+ "\n3. Play 2 Player Game" 
+	+ "\n4. See High Score" + "\n5. Quit" 
+	+ "\nEnter your choice =>");
+
+	private static final Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
-  Scanner scanner = new Scanner(System.in);
-  AccountManager accountManager = new AccountManager();
-  LogStuff("Game initialized.");
-  
-
-
-  int SelectedMenuOpt;
-  while (true) {
-	  login();
-	  // after login shit just stops working idk how to fix it -eric
-        if(accountManager.isLoggedIn()) {
-            displayMenu();
-            
-                SelectedMenuOpt = scanner.nextInt(); 
-                
-            switch (SelectedMenuOpt) {
-                case 1:
-                	System.out.println("rules\n");
-                 //   displayRules();
-                	break;
-                case 2:
-                	System.out.println("1 player");
-                 //   playAgainstComputer();
-                    break;
-                case 3:
-                	System.out.println("2player");
-                   // playTwoPlayerGame();
-                    break;
-                case 4:
-                	System.out.println("high score");
-                  //  seeHighScore();
-                    break;
-                case 5:
-                    System.out.println("Exiting game.");
-                    break;
-                default:
-                    System.out.println("Please enter a number between 1 and 5.");
-            }
-        }
-  
-        scanner.close();
-  }	
- }
-
-public static void displayMenu(){
-	System.out.println("Main Menu");
-	System.out.println("1. Rules");
-	System.out.println("2. Play Against Computer");
-	System.out.println("3. Play 2 Player Game");
-	System.out.println("4. See High Score");
-	System.out.println("5. Quit");
-	System.out.print("Enter your choice: ");
-
+		AccountManager manager = new AccountManager();
+		LogStuff("Game initialized.");
+		
+		login();
+		if (manager.isLoggedIn()) {
+			LogStuff("Logged in.");
+		}
+		loop();
 	}
 
-public static void login() {
-    Scanner scanner = new Scanner(System.in);
-    AccountManager accountManager = new AccountManager();
+	public static void loop() {	
 
-    System.out.println("1. Create Account");
-    System.out.println("2. Login");
-    System.out.println("Enter your choice:");
-
-    int choice = scanner.nextInt();
-    scanner.nextLine();
-
-    switch (choice) {
-        case 1:
-            System.out.println("Enter username:");
-            String newUsername = scanner.nextLine();
-            System.out.println("Enter password:");
-            String newPassword = scanner.nextLine();
-            accountManager.createUser(newUsername, newPassword);
-            break;
-        case 2:
-            System.out.println("Enter username:");
-            String username = scanner.nextLine();
-            System.out.println("Enter password:");
-            String password = scanner.nextLine();
-            accountManager.loginUser(username, password);
-            break;
-        default:
-            System.out.println("Invalid choice");
-            break;
-    }
-
-    scanner.close();
-}
-public static void LogStuff(String data) {
-	File log = new File("c:\\temp\\log.txt");
-	
-	try (BufferedWriter bw = new BufferedWriter(new FileWriter(log, true))) {	
-		bw.write(data);
-        bw.newLine();  
-		bw.close();
+		System.out.println(displayMenu);
+		;
+		int selection = sc.nextInt();
+		switch (selection) {
+		case 1:
+			System.out.println("rules\n");
+			System.out.println("return to menu?");
+			int ch = sc.nextInt();
+			if (ch == 1) {
+				break;
+			}
+			return;
+		case 2:
+			System.out.println("1 player");
+			break;
+		case 3:
+			System.out.println("2player");
+			break;
+		case 4:
+			System.out.println("high score");
+			break;
+		case 5:
+			System.out.println("Exiting game.");
+			sc.close();
+			return;
+		}
+		loop();
 	}
- 	catch(IOException ex){
- 	   ex.printStackTrace();
+//	}
+
+	public static void login() {
+		AccountManager accountManager = new AccountManager();
+
+		System.out.println("1. Create Account");
+		System.out.println("2. Login");
+		System.out.println("Enter your choice:");
+
+		int choice = sc.nextInt();
+		sc.nextLine();
+
+		switch (choice) {
+		case 1:
+			System.out.println("Enter username:");
+			String newUsername = sc.nextLine();
+			System.out.println("Enter password:");
+			String newPassword = sc.nextLine();
+			accountManager.createUser(newUsername, newPassword);
+			break;
+		case 2:
+			System.out.println("Enter username:");
+			String username = sc.nextLine();
+			System.out.println("Enter password:");
+			String password = sc.nextLine();
+			accountManager.loginUser(username, password);
+			break;
+
+		default:
+			System.out.println("Invalid choice");
+			break;
+		}
 	}
-    }
+
+	public static void LogStuff(String data) {
+		File log = new File("c:\\temp\\log.txt");
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(log, true))) {
+			bw.newLine();
+			bw.write(data);
+			bw.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
